62302c8f326ebcda36f62eeea91c51588be1ab98
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-09-01 14:04:42 +0200

    task 2

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import routes from './routes/index.js';
+
+const app = express();
+
+app.use('/', routes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+export default app;
